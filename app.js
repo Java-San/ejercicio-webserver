d551e64f67915148946d479ca53539f4a7f1f640
Javiera Quiñones <jquinones@andain.cl>
2021-10-18 12:39:20 -0300

    Fin proyecto.

---
+++ app.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+const express = require('express');
+const hbs = require('hbs');
+
+const app = express();
+const port = process.env.PORT;
+
+app.set('view engine', 'hbs');
+hbs.registerPartials( __dirname + '/views/partials' )
+
+app.use( express.static( 'public' ) );
+
+app.get('/', (req, res) =>  {
+  //res.sendFile( __dirname + '/public/index.html' );
+
+  res.render( 'home', {
+	  nombre: 'Javiera Quiñones',
+	  titulo: 'Curso Node'
+  } );
+});
+
+app.get( '/generic', ( req, res ) => {
+    // res.sendFile( __dirname + '/public/generic.html' );
+
+	res.render( 'generic', {
+		nombre: 'Javiera Quiñones',
+		titulo: 'Curso Node'
+	} );
+} );
+
+app.get( '/elements', ( req, res ) => {
+    // res.sendFile( __dirname + '/public/elements.html' );
+
+	res.render( 'elements', {
+		nombre: 'Javiera Quiñones',
+		titulo: 'Curso Node'
+	} );
+} );
+
+app.get( '*', ( req, res ) => {
+    res.sendFile( __dirname + '/public/404.html' );
+} );
+
+app.listen( port, () => {
+	console.log( `Escuchando en el puerto: ${port}` );
+} );
